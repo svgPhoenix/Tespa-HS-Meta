07d52661b7ad8c0dc3a4c4ef393fe9532a4bda0c
svgPhoenix <immadeofpixels27@gmail.com>
2018-10-07 10:15:33 -0500

    Add project files.

---
+++ TESPA HS Meta/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace SheetsQuickstart
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
